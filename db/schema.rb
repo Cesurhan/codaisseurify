aa67748ba3f9dbf4aea6402bb5a623fc88c727e0
Cesurhan <ces-ur@hotmail.com>
2017-03-18 16:30:01 +0100

    Declare the model relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318152012) do
+ActiveRecord::Schema.define(version: 20170318152616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20170318152012) do
     t.string   "nomination"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
+  add_foreign_key "songs", "artists"
 end
