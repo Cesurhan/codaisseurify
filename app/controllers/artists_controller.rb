b41bb8dfdc343be4b86c1f9b13124fcda6462ab8
Cesurhan <ces-ur@hotmail.com>
2017-03-19 18:09:44 +0100

    Refactor

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,9 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
-    @photos = Photo.all
   end
 
   def show
     @artist = Artist.find(params[:id])
-    @photos = Photo.all
   end
 end
