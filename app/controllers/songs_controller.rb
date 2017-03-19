f800cd318149825a4a4414fa376cd2a274fde3eb
Cesurhan <ces-ur@hotmail.com>
2017-03-19 23:38:55 +0100

    Add more styling

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,16 @@ class SongsController < ApplicationController
   def new
     @song = Song.new
   end
+
+  def create
+    song_params = params.require(:song).permit(:title, :album, :release_date, :award)
+
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song, notice: 'Song successfully created'
+    else
+      render :new, notice: 'Please fill all the fields'
+    end
+  end
 end
