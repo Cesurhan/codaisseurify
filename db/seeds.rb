6b220b3f5ff199852d415b177dedba164270b110
Cesurhan <ces-ur@hotmail.com>
2017-03-19 12:34:26 +0100

    Generate photo model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,22 @@
+Photo.delete_all
 Song.delete_all
 Artist.delete_all
 
+# Photos
+photo1 = Photo.create!(remote_image_url: 'http://res.cloudinary.com/dr08wglx5/image/upload/v1489921746/440px-Ed_Sheeran_2013_yntsng.jpg')
+photo2 = Photo.create!(remote_image_url: 'http://res.cloudinary.com/dr08wglx5/image/upload/v1489921811/440px-Sia_Seattle__28cropped_29_kswdnn.jpg')
+photo3 = Photo.create!(remote_image_url: 'http://res.cloudinary.com/dr08wglx5/image/upload/v1489921848/300px-Danny_Tenaglia_cropped_ayl1jd.jpg')
+
+puts "#{Photo.all.size} photos created"
+
 # Artists
-ed = Artist.create!(first_name: 'Ed', last_name: 'Sheeran', age: 26, origin: 'England')
-sia = Artist.create!(first_name: 'Sia', last_name: 'Kate Isobelle Furler', age: 41, origin: 'Australia')
-celeda = Artist.create!(first_name: 'Danny', last_name: 'Tenaglia', age: 56, origin: 'United States')
+ed = Artist.create!(first_name: 'Ed', last_name: 'Sheeran', age: 26, origin: 'England', photos: [photo1])
+sia = Artist.create!(first_name: 'Sia', last_name: 'Kate Isobelle Furler', age: 41, origin: 'Australia', photos: [photo2])
+celeda = Artist.create!(first_name: 'Danny', last_name: 'Tenaglia', age: 56, origin: 'United States', photos: [photo3])
 
 puts "#{Artist.all.size} artists created"
 
+# Songs
 Song.create!([
                {
                  title: 'I see fire',
